bed923a54d0e540bedf896cb061797827c329c2f
lvlugov405-design <lvlugov405@gmail.com>
2025-12-10 17:50:32 -0500

    Add files via upload
    escalera java

---
+++ Escalera.java
@@ -0,0 +1,22 @@
+public class Escalera{
+
+public static void main(String[] args){
+int a
+int b
+
+a=10;
+
+	For( int i=1; i=a; i++){
+		For( b=1; b=i; b++){
+		System.out.println(  "*"," ");
+		}
+		System.out.println("");
+
+	}
+
+				
+}
+
+
+}
+
